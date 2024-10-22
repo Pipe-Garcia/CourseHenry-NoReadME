bb8a2a27d46750e137f29f1c3bf6b02fa4ae5e39
Pipe-Garcia <pipeg1069@gmail.com>
2024-10-22 20:20:04 -0300

    funciones

---
+++ Funciones2.js
@@ -0,0 +1,15 @@
+function cuidadoConElConsoleLog(nombre) {
+    console.log(nombre);
+    return nombre;
+}
+
+function otraFuncion() {
+    return (
+        "El nombre retornado por la funcion 'cuidadoConElConsoleLog' es: " + cuidadoConElConsoleLog('Felipe')
+    );
+}
+
+function cuidadoConElReturn(nombre) {
+    return nombre;
+    console.log(nombre);
+}
